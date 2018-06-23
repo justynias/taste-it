d564ead5cbd5054c2e2a4719754cfeb407ea4d34
justynias <justynakozik1@gmail.com>
2018-06-24 01:32:07 +0200

    initial vm

---
+++ taste-it/ViewModels/AddRecipeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using taste_it.Additionals.NavigationService;
+using taste_it.DataService;
+using taste_it.Models;
+
+namespace taste_it.ViewModels
+{
+   
+    public class AddRecipeViewModel : ViewModelBase
+    {
+        private readonly IUserDataService _userDataService;
+        public AddRecipeViewModel()
+        {
+            
+        }
+    }
+}
