20e2e64010ab6c579c955c15a03174699a2cf65f
Damian Fikier <damian.fikier@wp.pl>
2018-06-24 00:59:34 +0200

    Category and Tag Data Services

---
+++ taste-it/DataService/CategoryDataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using taste_it.Models;
+
+namespace taste_it.DataService
+{
+    public class CategoryDataService : ICategoryDataService
+    {
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            var dbContext = new TasteItDbEntities();
+            return await dbContext.Categories.AsNoTracking().ToListAsync();
+        }
+    }
+}
---
+++ taste-it/DataService/TagDataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using taste_it.Models;
+
+namespace taste_it.DataService
+{
+    public class TagDataService : ITagDataService
+    {
+        public async Task AddTagAsync(Tag tag)
+        {
+            var dbContext = new TasteItDbEntities();
+            dbContext.Tags.Add(tag);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Tag>> GetTagsAsync()
+        {
+            var dbContext = new TasteItDbEntities();
+            return await dbContext.Tags.AsNoTracking().ToListAsync();
+        }
+    }
+}
