409a6aaa8998086cc6caca0fc08570c7b90e42e1
luksari <luksari23@gmail.com>
2018-06-28 19:42:58 +0200

    Design changes for working sidebar

---
+++ taste-it/ViewModels/SidebarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taste_it.ViewModels
+{
+    public class SidebarViewModel : ViewModelBase
+    {
+
+    }
+}
