f2c412e0037bfb043b8f440b016fdbe3e3c429ca
luksari <luksari23@gmail.com>
2018-06-28 19:53:16 +0200

    Design rewrite. Some template vm

--- taste-it/ViewModels/SidebarViewModel.cs
+++ taste-it/ViewModels/SidebarViewModel.cs
@@ -4,11 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using taste_it.Additionals.ContentNavigationService;
 
 namespace taste_it.ViewModels
 {
     public class SidebarViewModel : ViewModelBase
     {
+        private string message; // Message shown to user with his/her name e.g Hello root
+        private IPageViewModel currentPageViewModel;
+        private List<IPageViewModel> pageViewModels; // Switching between filterview and TASTE.IT view
+        public SidebarViewModel() // Here we have to pass user probably from NavigableContentViewModel
+        {
 
+        }
     }
 }
