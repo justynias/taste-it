66260bda4a063379ed7d81d1d21f065af3b00bef
luksari <luksari23@gmail.com>
2018-07-02 19:06:25 +0200

    Filter Fav recipes

--- taste-it/ViewModels/FavouriteRecipesViewModel.cs
+++ taste-it/ViewModels/FavouriteRecipesViewModel.cs
@@ -21,6 +21,11 @@ namespace taste_it.ViewModels
         private User _currentUser;
         private IRecipeDataService _recipeDataService;
         private ObservableCollection<Recipe> recipesCollection;
+        private ObservableCollection<Recipe> filteredRecipesCollection;
+
+        private string filterRecipeName = String.Empty;
+        private ObservableCollection<Tag> filterTags;
+        private ObservableCollection<Category> filterCategories;
         public ICommand AddRecipeToFavouritesCommand { get; private set; }
         public ICommand RemoveRecipeToFavouritesCommand
         { get; private set; }
@@ -45,16 +50,154 @@ namespace taste_it.ViewModels
             }
         }
 
+        public ObservableCollection<Recipe> FilteredRecipesCollection
+        {
+            get
+            {
+                return filteredRecipesCollection;
+            }
+
+            set
+            {
+                Set(ref filteredRecipesCollection, value);
+            }
+        }
+
         public FavouriteRecipesViewModel(IRecipeDataService recipeData)
         {
             _recipeDataService = recipeData;
-            Messenger.Default.Register<CurrentUserMessage>(this, this.HandleCurrentUserMessage);
+
             RecipesCollection = new ObservableCollection<Recipe>();
+            FilteredRecipesCollection = new ObservableCollection<Recipe>();
+            filterTags = new ObservableCollection<Tag>();
+            filterCategories = new ObservableCollection<Category>();
+
             AddRecipeToFavouritesCommand = new RelayCommand<object>(AddRecipeToFavourites);
             RemoveRecipeToFavouritesCommand = new RelayCommand<object>(RemoveRecipeToFavourites);
+
+            Messenger.Default.Register<CurrentUserMessage>(this, this.HandleCurrentUserMessage);
+            Messenger.Default.Register<FiltersMessage>(this, this.HandleFiltersMessage);
         }
 
-      private async void LoadRecipes() //when view is loading after getting message with current user
+        private void HandleFiltersMessage(FiltersMessage message)
+        {
+            this.filterCategories = message.FilterCategories;
+            this.filterTags = message.FilterTags;
+            this.filterRecipeName = message.FilterName;
+            Filter();
+
+        }
+        private void Filter()
+        {
+            List<Recipe> filteredRecipes = new List<Recipe>();
+
+            if (filterRecipeName != string.Empty)
+            {
+
+                if (filterCategories.Count() == 0 && filterTags.Count == 0)
+                {
+                    var filteredByName = FilterByName();
+                    filteredRecipes.AddRange(filteredByName);
+                }
+                else if (filterCategories.Count() > 0 && filterTags.Count == 0)
+                {
+                    var filteredByCategories = FilterByCategories();
+                    var filteredByName = FilterByName();
+                    filteredRecipes.AddRange(filteredByCategories.Intersect(filteredByName));
+                }
+                else if (filterCategories.Count() == 0 && filterTags.Count > 0)
+                {
+                    var filteredByName = FilterByName();
+                    var filteredByTags = FilterByTags();
+                    filteredRecipes.AddRange(filteredByName.Intersect(filteredByTags));
+                }
+                else
+                {
+                    var filteredByCategories = FilterByCategories();
+                    var filteredByName = FilterByName();
+                    var filteredByTags = FilterByTags();
+                    HashSet<Recipe> hashSet = new HashSet<Recipe>(filteredByCategories);
+                    hashSet.IntersectWith(filteredByName);
+                    hashSet.IntersectWith(filteredByTags);
+                    filteredRecipes.AddRange(hashSet.ToList());
+                }
+            }
+            else if (filterTags.Count > 0)
+            {
+                if (filterCategories.Count > 0)
+                {
+                    var filteredByCategories = FilterByCategories();
+                    var filteredByTags = FilterByTags();
+                    filteredRecipes.AddRange(filteredByCategories.Intersect(filteredByTags));
+                }
+                else
+                {
+                    var filteredByTags = FilterByTags();
+                    filteredRecipes.AddRange(filteredByTags);
+
+                }
+            }
+            else if (filterCategories.Count > 0)
+            {
+                var filteredByCategories = FilterByCategories();
+                filteredRecipes.AddRange(filteredByCategories);
+            }
+            else // without filters
+            {
+                filteredRecipes.AddRange(RecipesCollection);
+
+            }
+            FilteredRecipesCollection.Clear();
+            foreach (var item in filteredRecipes)
+            {
+                FilteredRecipesCollection.Add(item);
+            }
+            RaisePropertyChanged(() => FilteredRecipesCollection);
+
+
+        }
+
+        private List<Recipe> FilterByCategories()
+        {
+            List<Recipe> filteredRecipes = new List<Recipe>();
+            foreach (var category in filterCategories)
+            {
+                foreach (var r in RecipesCollection)
+                {
+                    if (r.Have_category.Any(c => c.id_c == category.id_c))
+                    {
+
+                        filteredRecipes.Add(r);
+                    }
+                }
+            }
+            return filteredRecipes;
+        }
+
+        private List<Recipe> FilterByName()
+        {
+            var filteredRecipes = RecipesCollection.Where(r => r.name.ToLower().Contains(filterRecipeName.ToLower())).ToList();
+            return filteredRecipes;
+        }
+
+        private List<Recipe> FilterByTags()
+        {
+            List<Recipe> filteredRecipes = new List<Recipe>();
+            foreach (var tag in filterTags)
+            {
+                foreach (var r in RecipesCollection)
+                {
+                    if (r.Have_tags.Any(t => t.Tag.name.ToLower() == tag.name.ToLower()))
+                    {
+                        filteredRecipes.Add(r);
+                    }
+                }
+
+            }
+            return filteredRecipes;
+        }
+
+        private async void LoadRecipes() //when view is loading after getting message with current user
         {
 
             var recipes = await _recipeDataService.FindFavouritesAsync(_currentUser);
@@ -67,8 +210,10 @@ namespace taste_it.ViewModels
                     item.isFavourite = true;
                 }
                 RecipesCollection.Add(item);
+                FilteredRecipesCollection.Add(item);
             }
             RaisePropertyChanged(() => RecipesCollection);
+            RaisePropertyChanged(() => FilteredRecipesCollection);
 
         }
         private void HandleCurrentUserMessage(CurrentUserMessage message)
